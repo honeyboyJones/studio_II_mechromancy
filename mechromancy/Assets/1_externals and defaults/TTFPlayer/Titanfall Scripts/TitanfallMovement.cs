30b4a6212c42d063127978b098d3208cdffd17f1
LeeMcGirr <89796414+LeeMcGirr@users.noreply.github.com>
2022-05-19 21:28:22 -0400

    Revert "Merge branch 'main' into spaceship_cockpit_redux"
    This reverts commit 533fe3f389fd8beb6487b228050046d4b83eb8ef, reversing
    changes made to 80bae4fac9e0706a8512ffabb7529daf639d5724.

--- mechromancy/Assets/1_externals and defaults/TTFPlayer/Titanfall Scripts/TitanfallMovement.cs
+++ mechromancy/Assets/1_externals and defaults/TTFPlayer/Titanfall Scripts/TitanfallMovement.cs
@@ -112,7 +112,7 @@ public class TitanfallMovement : MonoBehaviour
     //Audio
     public AudioData audioData;
     public AudioSource PlayerAudio;
-    public AudioClip a_Walk, a_Jump, a_DJump, a_Land,a_Air,a_Sprint;
+    public AudioClip a_Walk, a_Jump, a_DJump, a_Land,a_Air;
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -126,8 +126,6 @@ public class TitanfallMovement : MonoBehaviour
         a_DJump = audioData.FectchAudio_DJump();
         a_Land = audioData.FectchAudio_Land();
         a_Air = audioData.FectchAudio_Air();
-        a_Sprint = audioData.FectchAudio_Sprint();
-
 
         //a_Walk.loop = enabled;
     }
@@ -209,10 +207,6 @@ public class TitanfallMovement : MonoBehaviour
             if (!running)
             {
                 running = true;
-                if (!PlayerAudio.isPlaying)
-                {
-                    PlayerAudio.PlayOneShot(a_Sprint, 0.5f);
-                }
             }
             else
             {
@@ -857,7 +851,7 @@ public class TitanfallMovement : MonoBehaviour
 
     void HandleHeadbob(Vector3 dir, float BodScaleX, float BodScaleY)
     {
-       // Debug.Log(mode);
+        Debug.Log(mode);
         if (Mathf.Abs(dir.magnitude)>0)
         {
             timer += Time.deltaTime * (crouched? crouchBobSpeed:running||mode==Mode.Wallruning? sprintBobSpeed:walkBobSpeed) ;
